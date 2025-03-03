d60b988ede21df1f87acba025a8fdd90a6ed8adf
helplanes <helplanes@gmail.com>
2025-03-03 21:40:30 +0530

    hardcoded emailJS

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -31,9 +31,10 @@ import {
 import { toast } from "sonner";
 
 // 2) (Optional) Read environment variables
-const SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!;
-const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!;
-const PUBLIC_KEY = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!;
+// Just hardcode them for GitHub Pages
+const SERVICE_ID = "service_sv5xc0e";
+const TEMPLATE_ID = "template_c0sb9j6";
+const PUBLIC_KEY = "KIDBTpyN0Te6ZQfpt";
 
 // 3) Define Zod schema
 const formSchema = z.object({
